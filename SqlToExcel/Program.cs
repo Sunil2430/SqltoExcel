d5ed7faf2606830e3279f813f46b088f6f1008bc
Sunil2430 <msunil1922@gmail.com>
2020-05-03 18:33:05 +0530

    first commit

---
+++ SqlToExcel/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace SqlToExcel
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Program s1 = new Program();
+            s1.ExportToExcel();
+        }
+
+        public void ExportToExcel()
+        {
+            SqlConnection cnn;
+            string connectionString = null;
+            string sql = null;
+            string data = null;
+            int i = 0;
+            int j = 0;
+
+            Excel.Application xlApp;
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            object misValue = System.Reflection.Missing.Value;
+
+            xlApp = new Excel.Application();
+            xlWorkBook = xlApp.Workbooks.Add(misValue);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            string FileName = @"D:\DemoSqlToExcel.xlsx";   // Change the file name
+            connectionString = "data source=SUNIL\\SQLEXPRESS;initial catalog=EmployeeDB;user id=sa;password=admin;";   // Connectionstring
+            cnn = new SqlConnection(connectionString);
+            cnn.Open();
+            sql = "select * from tblEmployee";  // query
+            SqlDataAdapter dscmd = new SqlDataAdapter(sql, cnn);
+            DataSet ds = new DataSet();
+            dscmd.Fill(ds);
+
+            for (i = 0; i <= ds.Tables[0].Rows.Count - 1; i++)
+            {
+                for (j = 0; j <= ds.Tables[0].Columns.Count - 1; j++)
+                {
+                    data = ds.Tables[0].Rows[i].ItemArray[j].ToString();
+                    xlWorkSheet.Cells[i + 1, j + 1] = data;
+                }
+            }
+
+            if (File.Exists(FileName))
+            {
+                File.Delete(FileName);
+                xlWorkBook.SaveAs(FileName); //, Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+            }
+            else
+            {
+                xlWorkBook.SaveAs(FileName);
+                //xlWorkBook.SaveAs(FileName, Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+
+            }
+
+            xlWorkBook.Close(true, misValue, misValue);
+            xlApp.Quit();
+
+            releaseObject(xlWorkSheet);
+            releaseObject(xlWorkBook);
+            releaseObject(xlApp);
+
+        }
+
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                //MessageBox.Show("Exception Occured while releasing object " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+    }
+}
